6455c8fcb589216ae768292ffa0082a4c9c9da09
skaunov <skaunov@disroot.org>
2023-05-25 14:17:55 +0300

    Clean solution (for review)
    Some reflections on the code
    ====
    while cleaning to explain design decisions.
    ----
    
    I was suspecting that `ref` doesn't go through FFI well enough and
    tried to employ `#![feature(ptr_from_ref)]`.
    
    First attempt was to employ offered `fn` from _starter code._ Then
    `rust_analyzer` recommended to replace oneliner for less concise but
    more reliable intermediate variables approach
    ```rust
    fn open(_path: &str) -> Option<Self> {
        // let path_c = /* Vec<i8> */ to_c_string(_path).as_mut_ptr();
        // let result = unsafe{fopen(CString::new(_path).unwrap().as_ptr(), 'r' as i8 as *const c_char)};
        let path_c = CString::new(_path).unwrap();
        let mode = CString::new("r").unwrap();
        let result = unsafe{fopen(path_c.as_ptr(), mode.as_ptr())};
        // println!("{result:?}");
        if result.is_null() {None}
        else {Some(File{ stream: result})}
    }
    ```
    
    Here initial idea/challenge was to read string of any realistic length
    that this exercise app could encounter. It took me some time to
    understand that this approach doesn't lead to solution conceptually,
    as `FILE` streams work principially different. So I just settled with
    some arbitrary length for the line read from `FILE`.
    
    Also I really don't like the final `bytes` trick, especially
    as `le`/`be` is mostly guessing here since I'm busy only with one byte.
    
    ```rust
    /// Returns `None` if file couldn't been read (to `String`) OR it
    /// doesn't contain EOL.
    fn read_string(&mut self) -> Option<String> {
        let mut buffa = [i8::default(); 512];
        // let /* mut */ buffa = buffa.as_mut_ptr();
        // let mut result: Vec<i8> = Default::default();
        // looks like it's ok to get just long enough start of the line; let it be 512 symbols
        // loop {
            let success = unsafe{fgets(buffa.as_mut_ptr() as *mut c_char, 510, self.stream)};
            if success.is_null() {return None}
            // let conca = unsafe{Vec::from_raw_parts(buffa, 8, 8)};
            // result = [result, conca].concat();
            // if conca.contains(&0) {
                // let result = &result[..=result.iter().position(|x| x == &0).unwrap()];
                // let result = result.into_iter().map(|x| x as c_char).collect();
                // println!("{:?}", unsafe{CString::from_raw(buffa.as_mut_ptr() as *mut c_char)});
    
                // let cstr = unsafe{CString::new(buffa)};
                // println!("{}", cstr.to_string_lossy());
                // Some(cstr.to_string_lossy().to_string())
    
                // TODO need not only skip 0, but also everything after first occurence
                Some(buffa.into_iter().map(|x| x.to_ne_bytes()[0] as u8).skip_while(|x| x == &0).map(|x| x as char).collect())
    
                // match cstr.into_string() {
                //     Ok(r) => {
                //         // println!("{r}");
                //         Some(r)
                //     }
                //     Err(_) => None,
                // }
                // Some(unsafe{String::from_utf8_unchecked(result)})
            // }
        // }
    }
    ```
    
    Tried to employ `CStr`, but couldn't find any benefits of it here
    over `CString`.
    ```rust
    fn read_char(&mut self) -> Option<char> {
        let mut result: c_char = Default::default();
        let mode = CString::new(" %c").unwrap();
        match unsafe{fscanf(self.stream, mode.as_ptr(), &mut result)} {
            EOF | 0 => None,
            _ => {
                // let result = unsafe{CStr::from_bytes_with_nul_unchecked(&result.to_be_bytes())};
                Some(result.to_be_bytes()[0].into())
            }
        }
    }
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,88 @@
-use libc::{fopen, fgets, fscanf, fclose, FILE, c_char, c_int, c_double};
+use libc::{c_char, c_double, c_int, fclose, fgets, fopen, fscanf, EOF, FILE};
+use std::ffi::CString;
 
 struct File {
-    // todo!()
+    stream: *mut FILE,
 }
 
-/// This function converts a string into a Vec<i8> which can
-/// be used to represent a c-string.
-///
-/// To turn this into a *mut c_char, use Vec<i8>::as_mut_ptr().
-fn to_c_string(string: &str) -> Vec<i8> {
-    let bytes: Vec<u8> = String::from(string).into_bytes();
-    let mut c_chars: Vec<i8> = bytes.iter().map(| c | *c as i8).collect();
-
-    c_chars.push(0); // null terminator
-
-    c_chars
-}
+/* At this place _starter_ code contains `to_c_string(string: &str) -> Vec<i8>` function, which I replaced in spite
+of `ffi::CString` methods during debugging, and didn't want come from this standard solution. */
 
 impl File {
     fn open(_path: &str) -> Option<Self> {
-        todo!()
+        let path_c = CString::new(_path).expect("it's exercise --- `_path` should be there");
+        let mode = CString::new("r").unwrap();
+        let result = unsafe { fopen(path_c.as_ptr(), mode.as_ptr()) };
+        if result.is_null() {
+            None
+        } else {
+            Some(File { stream: result })
+        }
     }
 
+    /// Returns `None` if file couldn't been read (to `String`) OR it
+    /// doesn't contain EOL.
     fn read_string(&mut self) -> Option<String> {
-        todo!()
+        let mut buffer_current = [i8::default(); 512];
+        // looks like it's ok to get just long enough start of the line; let it be 512 symbols
+        let success = unsafe {
+            fgets(
+                buffer_current.as_mut_ptr() as *mut c_char,
+                // note two bytes for EOL symbols in `buf`
+                510,
+                self.stream,
+            )
+        };
+        if success.is_null() {
+            return None;
+        }
+        Some(
+            buffer_current
+                .into_iter()
+                .map(|x| x.to_ne_bytes()[0] as u8)
+                .skip_while(|x| x == &0)
+                .map(|x| x as char)
+                .collect(),
+        )
     }
 
     fn read_i64(&mut self) -> Option<i64> {
-        todo!()
+        let mut result: c_int = Default::default();
+        let mode = CString::new("%d").unwrap();
+        match unsafe { fscanf(self.stream, mode.as_ptr(), &mut result) } {
+            EOF | 0 => None,
+            _ => Some(result.into()),
+        }
     }
 
     fn read_f64(&mut self) -> Option<f64> {
-        todo!()
+        let mut result: c_double = Default::default();
+        let mode = CString::new("%lf").unwrap();
+        match unsafe { fscanf(self.stream, mode.as_ptr(), &mut result) } {
+            EOF | 0 => None,
+            _ => Some(result.into()),
+        }
     }
 
     fn read_char(&mut self) -> Option<char> {
-        todo!()
+        let mut result: c_char = Default::default();
+        let mode = CString::new(" %c").unwrap();
+        match unsafe { fscanf(self.stream, mode.as_ptr(), &mut result) } {
+            EOF | 0 => None,
+            _ => Some(result.to_be_bytes()[0].into()),
+        }
     }
 }
 
 impl Drop for File {
     fn drop(&mut self) {
         println!("Dropping file.");
+        let success = unsafe { fclose(self.stream) };
+        if success != 0 {
+            panic!("...")
+        }
     }
 }
-
 
 fn main() {
     let mut file = File::open("data/test_file.txt").expect("Could not open file.");
